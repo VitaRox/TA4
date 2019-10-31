90d09668bfc27b51a3c40fc712b7757f67e5225b
Vita <wiebetc@gmail.com>
2019-10-30 18:59:12 -0700

    - Initial commit;

---
+++ src/VitaWiebeTA4.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class VitaWiebeTA4 {
+
+    // Create a selectionSort method and private helper method to recursively sort the arrays;
+
+
+
+    // Add code to get user input, build arrays, call selectionSort method;
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the number of students: ");
+
+        // Get number of students to be entered;
+        int students = scan.nextInt();
+
+        /*
+         Create parallel arrays, one to hold names and one to hold grades;
+         These shall lie at corresponding indices;
+        */
+        int [] nameArray = new int[students];
+        int [] gradeArray = new int[students];
+    }
+}
