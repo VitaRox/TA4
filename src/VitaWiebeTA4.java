f525b230b10ef11f512f0f03658dfc036354be9c
Vita <Vita.Wiebe@seattlecolleges.edu>
2019-10-30 19:33:42 -0700

    - OMG it works!!

--- src/VitaWiebeTA4.java
+++ src/VitaWiebeTA4.java
@@ -2,8 +2,30 @@ import java.util.*;
 
 public class VitaWiebeTA4 {
 
-    // Create a selectionSort method and private helper method to recursively sort the arrays;
+    /*
+     Create a selectionSort method and private helper method to recursively sort the arrays;
+     Void return type because is a mutator method (a.k.a. "setter");
+     Rewritten overloaded method with a param for the name list, since that will
+     have to be dealt with simultaneously;
+    */
+    static void selectionSort(double[] gradeList, String[] nameList){
 
+        for(int i = 0; i < gradeList.length; i++){
+            double currMax = gradeList[i];
+            int currMaxIndex = i;
+
+            for(int j = i+ 1; j < gradeList.length; j++){
+                if(currMax < gradeList[j]){
+                    currMax = gradeList[j];
+                    currMaxIndex = j;
+                }
+            }
+            if(currMaxIndex != i){
+                gradeList[currMaxIndex] = gradeList[i];
+                gradeList[i] = currMax;
+            }
+        }
+    }
 
 
     // Add code to get user input, build arrays, call selectionSort method;
@@ -35,10 +57,11 @@ public class VitaWiebeTA4 {
             double currGrade = scan.nextDouble();
             gradeArray[i] = currGrade;
         }
+        selectionSort(gradeArray,nameArray);
 
         System.out.println("You entered the following: ");
         for(int j = 0; j < nameArray.length; j++){
-            System.out.println("Name: " + nameArray[j] + "Grade: " + gradeArray[j]);
+            System.out.println("Name: " + nameArray[j] + "     Grade: " + gradeArray[j]);
         }
 
         System.out.println("Thanks for using my program and have a great day!");
